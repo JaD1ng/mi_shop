90a95f6d5d664ab8a6374a2bf2caaa8897f25df4
dnj <shdnj@qq.com>
2024-02-29 15:27:18 +0800

    access CRUD

--- controllers/admin/access.go
+++ controllers/admin/access.go
@@ -70,3 +70,83 @@ func (con AccessController) DoAdd(c *gin.Context) {
 	}
 	con.success(c, "增加数据成功", "/admin/access")
 }
+
+func (con AccessController) Edit(c *gin.Context) {
+	//获取要修改的数据
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		con.error(c, "参数错误", "/admin/access")
+	}
+	access := database.Access{Id: id}
+	database.DB.Find(&access)
+
+	//获取顶级模块
+	var accessList []database.Access
+	database.DB.Where("module_id=?", 0).Find(&accessList)
+
+	c.HTML(http.StatusOK, "admin/access/edit.html", gin.H{
+		"access":     access,
+		"accessList": accessList,
+	})
+}
+
+func (con AccessController) DoEdit(c *gin.Context) {
+	id, err1 := strconv.Atoi(c.PostForm("id"))
+	moduleName := strings.Trim(c.PostForm("module_name"), " ")
+	actionName := c.PostForm("action_name")
+	accessType, err2 := strconv.Atoi(c.PostForm("type"))
+	url := c.PostForm("url")
+	moduleId, err3 := strconv.Atoi(c.PostForm("module_id"))
+	sort, err4 := strconv.Atoi(c.PostForm("sort"))
+	status, err5 := strconv.Atoi(c.PostForm("status"))
+	description := c.PostForm("description")
+
+	if err1 != nil || err2 != nil || err3 != nil || err4 != nil || err5 != nil {
+		con.error(c, "传入参数错误", "/admin/access")
+		return
+	}
+	if moduleName == "" {
+		con.error(c, "模块名称不能为空", "/admin/access/edit?id="+strconv.Itoa(id))
+		return
+	}
+
+	access := database.Access{Id: id}
+	database.DB.Find(&access)
+	access.ModuleName = moduleName
+	access.Type = accessType
+	access.ActionName = actionName
+	access.Url = url
+	access.ModuleId = moduleId
+	access.Sort = sort
+	access.Description = description
+	access.Status = status
+
+	err := database.DB.Save(&access).Error
+	if err != nil {
+		con.error(c, "修改数据", "/admin/access/edit?id="+strconv.Itoa(id))
+		return
+	}
+	con.success(c, "修改数据成功", "/admin/access")
+}
+
+func (con AccessController) Delete(c *gin.Context) {
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		con.error(c, "传入数据错误", "/admin/access")
+		return
+	}
+
+	//获取我们要删除的数据
+	access := database.Access{Id: id}
+	database.DB.Find(&access)
+	if access.ModuleId == 0 { //顶级模块
+		var accessList []database.Access
+		database.DB.Where("module_id = ?", access.Id).Find(&accessList)
+		if len(accessList) > 0 {
+			con.error(c, "当前模块下面有菜单或者操作，请删除菜单或者操作以后再来删除这个数据", "/admin/access")
+			return
+		}
+	}
+	database.DB.Delete(&access)
+	con.success(c, "删除数据成功", "/admin/access")
+}
