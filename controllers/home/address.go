e3265a0c8cffb7923e4d6f6f1fef51cee7b1d176
dnj <shdnj@qq.com>
2024-03-17 16:35:15 +0800

    edit address

--- controllers/home/address.go
+++ controllers/home/address.go
@@ -1,6 +1,8 @@
 package home
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"mi_shop/database"
 	"mi_shop/util"
@@ -55,22 +57,76 @@ func (con AddressController) AddAddress(c *gin.Context) {
 
 // GetOneAddressList 获取一个收货地址  返回指定收货地址id的收货地址
 func (con AddressController) GetOneAddressList(c *gin.Context) {
-	c.String(200, " 获取一个收货地址")
+	// 1、获取addressId
+	addressId, err := strconv.Atoi(c.Query("addressId"))
+	if err != nil {
+		c.JSON(200, gin.H{
+			"success": false,
+			"message": "参数错误",
+		})
+		return
+	}
+
+	// 2、获取用户id
+	user := database.User{}
+	util.Cookie.Get(c, "userinfo", &user)
+
+	// 3、查询当前addressId  userID对应的数据
+	var addressList []database.Address
+	database.DB.Where("id = ? AND uid = ?", addressId, user.Id).Find(&addressList)
+	if len(addressList) > 0 {
+		c.JSON(200, gin.H{
+			"success": true,
+			"result":  addressList[0],
+		})
+
+	} else {
+		c.JSON(200, gin.H{
+			"success": false,
+			"message": "参数错误",
+		})
+		return
+	}
 }
 
 // EditAddress 编辑收货地址
 func (con AddressController) EditAddress(c *gin.Context) {
-	/*
-	   1、获取表单增加的数据
+	// 1、获取用户信息以及 表单修改的数据
+	user := database.User{}
+	util.Cookie.Get(c, "userinfo", &user)
+	id, err := strconv.Atoi(c.PostForm("id"))
+	if err != nil {
+		c.JSON(200, gin.H{
+			"success": false,
+			"message": "参数错误",
+		})
+		return
+	}
 
-	   2、更新当前用户的所有收货地址的默认收货地址状态为0
+	name := c.PostForm("name")
+	phone := c.PostForm("phone")
+	address := c.PostForm("address")
 
-	   3、修改当前收货地址，让默认收货地址状态是1
+	// 2、更新当前用户的所有收货地址的默认收货地址状态为0
+	database.DB.Table("address").Where("uid = ?", user.Id).Updates(map[string]any{"default_address": 0})
 
-	    4、查询当前用户的所有收货地址并返回
+	// 3、修改当前收货地址，让默认收货地址状态是1
+	editAddress := database.Address{Id: id}
+	database.DB.Find(&editAddress)
+	editAddress.Name = name
+	editAddress.Phone = phone
+	editAddress.Address = address
+	editAddress.DefaultAddress = 1
+	database.DB.Save(&editAddress)
 
-	*/
-	c.String(200, " 编辑收货地址")
+	// 4、返回当前用户的所有收货地址返回
+	var addressList []database.Address
+	database.DB.Where("uid = ?", user.Id).Order("id desc").Find(&addressList)
+
+	c.JSON(200, gin.H{
+		"success": true,
+		"result":  addressList,
+	})
 }
 
 // ChangeDefaultAddress 修改默认的收货地址
